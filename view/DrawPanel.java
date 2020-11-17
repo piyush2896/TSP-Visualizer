6f5800f065eda87a1865069907631f261a5398f5
Piyush Malhotra <piyushmalhotra28@gmail.com>
2020-11-16 23:54:58 -0700

    DrawPanel Update

--- view/DrawPanel.java
+++ view/DrawPanel.java
@@ -3,5 +3,16 @@ package view;
 import javax.swing.*;
 
 public class DrawPanel extends JPanel {
-    
+    private JFileChooser fileChooser;
+    private JMenu fileMenu;
+    private JMenu projectMenu;
+    private JMenu aboutMenu;
+
+    private JMenuItem openMenuItem;
+    private JMenuItem saveMenuItem;
+    private JMenuItem runMenuItem;
+    private JMenuItem stopMenuItem;
+    private JMenuItem newMenuItem;
+
+    private JMenuBar mainMenuBar;
 }
